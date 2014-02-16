353aa95acd480275e340c60c07e449615858ac42
lunch <lunch@lunch-laptop.(none)>
2014-02-17 01:56:47 +0800

    define data area

--- ftserver.py
+++ ftserver.py
@@ -2,18 +2,25 @@
 
 import BaseHTTPServer
 import sys
+import os
 
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
-    dataPath = 'data/'
 
     def do_GET(self):
-        self.wfile.write(self.dataPath)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write("123")
+
 
 def RunServer(port):
+    fileName = os.path.abspath(sys.argv[0])
+    filePath = os.path.dirname(fileName)
+    os.chdir(filePath + '/data')
     server = BaseHTTPServer.HTTPServer(('', port), Handler)
-    print 'Enter ctrl+c to stop server'
+    print "Enter ctrl+c to stop server"
     server.serve_forever()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     RunServer(int(sys.argv[1]))
