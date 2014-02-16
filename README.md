68e178eef5ba04933043df762c56c9fc35288a61
FishYZ <fishlee.lyz@gmail.com>
2014-02-16 20:27:09 +0800

    1

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 This is a file transfer tool.
 
-<<<<<<< HEAD
-NEW LINE.
-
-fish add new line 19:08
-=======
 Get or put file only in the data folder.
->>>>>>> b2fa5d444342336d851a1a89409c13a17a5ba7bc
+
+
+
+Add a new line by lunch.
--- ftserver.py
+++ ftserver.py
@@ -1,3 +1,11 @@
 #!/use/bin/env python
 
-print 'hello'
+from SimpleHTTPServer import HTTPServer
+
+def run_server():
+    server = HTTPServer()
+    print 'Enter ctrl+c to stop server'
+    server.serve_forever()
+
+if __name__ == '__main__':
+    run_server()
