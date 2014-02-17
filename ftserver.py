deeb1e09697df4230e733b452bfb8f4d4c39e6b8
lunch <lunch@lunch-laptop.(none)>
2014-02-17 14:46:36 +0800

    page show

--- ftserver.py
+++ ftserver.py
@@ -3,6 +3,14 @@
 import BaseHTTPServer
 import sys
 import os
+import cgi
+import urllib
+import shutil
+import posixpath
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -10,9 +18,11 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     """Serve the GET request"""
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(self.path)
+        path = self.translate_path(self.path)
+        f = self.show_page(path)
+        if f:
+            shutil.copyfileobj(f, self.wfile)
+            f.close()
 
 
     """Serve the HEAD request"""
@@ -45,7 +55,46 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     """Use to list files and show the upload form"""
     def show_page(self, path):
-        pass
+        try:
+            list = os.listdir(path)
+        except os.error:
+            self.send_error(404, "No permission to list directory")
+            return None
+        list.sort(key=lambda a: a.lower())
+        f = StringIO()
+        displaypath = cgi.escape(urllib.unquote(self.path))
+        f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+        f.write('<html>\n<title>Directory listing for %s</title>\n' % displaypath)
+        f.write('<body>\n<h2>Directory listing for %s</h2>\n' % displaypath)
+        f.write('<form method="post"')
+        f.write('enctype="multipart/form-data">')
+        f.write('<label for="file">Filename:</label>')
+        f.write('<input type="file" name="file" id="file" />')
+        f.write('<br />')
+        f.write('<input type="submit" name="submit" value="Submit" />')
+        f.write('</form>')
+        f.write('<hr>\n<ul>\n')
+        for name in list:
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+            if os.path.islink(fullname):
+                displayname = name + "@"
+                # Note: a link to a directory displays with @ and links with /
+            f.write('<li><a href="%s">%s</a>\n'
+                    % (urllib.quote(linkname), cgi.escape(displayname)))
+        f.write("</ul>\n<hr>\n</body>\n</html>\n")
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        encoding = sys.getfilesystemencoding()
+        self.send_header("Content-type", "text/html; charset=%s" % encoding)
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        return f
 
 
 def RunServer(port):
